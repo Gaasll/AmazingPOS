2ea64c8cce7d8bc5d9c0974635fcf21c8bfddc4b
Garabet Aslo <98517170+Gaasll@users.noreply.github.com>
2022-09-09 08:54:07 +0200

    Add files via upload

---
+++ main/java/se/kth/iv1350/amazingpos/integration/InvalidItemException.java
@@ -0,0 +1,20 @@
+package se.kth.iv1350.amazingpos.integration;
+
+
+public class InvalidItemException extends Exception {
+    /**
+     * Creates a new instance of InvalidItemIdException.
+     * @param itemId represents the item id.
+     */
+    public InvalidItemException(int itemID){
+        super("The item with id: " + itemID + " can't be found.");
+    }
+
+    public int getIncorrectID() {
+        return 0;
+    }
+
+    public String getAdminMessage() {
+        return null;
+    }
+}
